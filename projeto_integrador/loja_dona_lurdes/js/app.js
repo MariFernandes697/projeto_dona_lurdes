7eff3d85084b110d1ee3a2b5a426bdba060f9cac
Marianna Fernandes <mariannafernandes697@gmail.com>
2025-05-10 10:20:03 -0300

    ver produto

--- projeto_integrador/loja_dona_lurdes/js/app.js
+++ projeto_integrador/loja_dona_lurdes/js/app.js
@@ -10,3 +10,16 @@ function menucelular(){
         MenuItens.style.maxHeight = "0px";
     }
 }
+var produtoImg = document.getElementById("produtoImg");
+var produtoMiniatura = document.getElementsByClassName("miniatura-do-produto");7
+
+produtoMiniatura[0].onclick = function(){
+    produtoImg.src = produtoMiniatura[0].src;
+}
+produtoMiniatura[1].onclick = function(){
+    produtoImg.src = produtoMiniatura[1].src;
+}
+produtoMiniatura[2].onclick = function(){
+    produtoImg.src = produtoMiniatura[2].src;
+}
+
