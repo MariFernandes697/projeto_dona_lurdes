c1b54c19a52beef0bcc2a7089f4b04ba12056a03
Marianna Fernandes <mariannafernandes697@gmail.com>
2025-05-24 20:16:48 -0300

    tenativa dois

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+    <footer class="rodape">
+        <div class="container">
+            <div class="linha">
+                <!-- (Seu conteúdo do rodapé aqui) -->
+            </div>
+        </div>
+        <hr>
+        <p class="direitos">&#169; Todos os direitos reservados</p>
+    </footer>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="loja_dona_lurdes/js/app.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plataforma de Vendas Armazém do Sítio</title>
+    <link rel="stylesheet" href="loja_dona_lurdes/css/style.css">
+    <link rel="stylesheet" href="loja_dona_lurdes_produtos/css/style.css">
+</head>
+<body>
+    <div class="novoMenu">
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="loja_dona_lurdes/projeto/arquivos-loja-v-1/img/logo_principal.png" alt="Logo Dona Lurdes" width="110px" height="100px">
+                </div>
+                <nav>
+                    <ul id="MenuItens">
+                        <li><a href="index.php">Início</a></li>
+                        <li><a href="produtos.php">Produtos</a></li>
+                        <li><a href="">Contato</a></li>
+                        <li><a href="minha_conta.php">Minha Conta</a></li>
+                    </ul>
+                </nav>
+                <a href="carrinho.php"><img src="loja_dona_lurdes/projeto/arquivos-loja-v-1/img/carrinho-de_compras.png" width="60px" height="60px"></a>
+                <img src="loja_dona_lurdes/projeto/arquivos-loja-v-1/img/menu.png" class="menu-celular" onclick="menucelular()">
+            </div>
+        </div>
+    </div>
